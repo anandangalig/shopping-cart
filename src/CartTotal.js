2f90cf4b77d3cf5f6a435f6c50aef688131d6a2c
Anand Angalig <anandangalig@gmail.com>
2020-07-12 16:03:23 -0400

    add subtotal and discount to CartTotal component

--- src/CartTotal.js
+++ src/CartTotal.js
@@ -5,8 +5,23 @@ const CartTotal = ({ productsInCart }) => {
     accum += product.quantity * product.credit_coupon_price;
     return accum;
   }, 0);
+  const subtotal = productsInCart.reduce((accum, product) => {
+    accum += product.quantity * product.price;
+    return accum;
+  }, 0);
+  const discount = productsInCart.reduce((accum, product) => {
+    accum += product.discount;
+    return accum;
+  }, 0);
 
-  return <h1>Grand total: ${total.toFixed(2)}</h1>;
+  return (
+    <div>
+      <h3>Subtotal: ${subtotal.toFixed(2)}</h3>
+      <h3>Discount: ${discount.toFixed(2)}</h3>
+      <hr />
+      <h1>Grand total: ${total.toFixed(2)}</h1>
+    </div>
+  );
 };
 
 export default CartTotal;
